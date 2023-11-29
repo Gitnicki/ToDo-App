ca087b8269a8858e6fcba5944ae77bdedb39800f
BenTechstarter <benedict@docc.techstarter.de>
2023-11-29 11:08:02 +0100

    main.py

--- src/main.py
+++ src/main.py
@@ -1,15 +1,21 @@
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
-from fastapi import APIRouter, Request
+from fastapi import APIRouter, Request, FastAPI
 import mysql.connector 
 
-app =APIRouter(title="ToDo-List", version="0.0.1", openapi_url="/openapi.json")
-app.mount("/assets", StaticFiles(directory="assets"), name="assets")
+app =FastAPI(title="ToDo-List", version="0.0.1", openapi_url="/openapi.json")
 
 router = APIRouter()
 templates = Jinja2Templates(directory="./templates")
 
 @app.get('/', response_class=HTMLResponse)
-def read_root():
+def read_root(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
 
+# @app.post("/add-notes")
+# def post_notes("/"):
+#     return notes
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
