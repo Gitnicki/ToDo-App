4873b0325bf3422f455dfc1b4e411a9e1e65ab09
Gitnicki <Richter.Nicole.D@gmail.com>
2023-11-30 09:32:35 +0100

    repaired database and main.py

--- src/main.py
+++ src/main.py
@@ -19,13 +19,13 @@ def create_server_connection():
     user_name = config.get("DB_USER")
     user_password = config.get("DB_PASSWORD")
     db_name = config.get("DB_NAME")
-    # try:
-    #     connection = mysql.connector.connect(
-    #         host=host_name, user=user_name, passwd=user_password, database=db_name
-    #     )
-    #     print("MySQL Database connection successful")
-    # except Error as err:
-    #     print(f"Error: '{err}'")
+    try:
+        connection = mysql.connector.connect(
+            host=host_name, user=user_name, passwd=user_password, database=db_name
+        )
+        print("MySQL Database connection successful")
+    except Error as err:
+        print(f"Error: '{err}'")
 
     return connection
 
